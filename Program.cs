905797ce35502cf3cb973ecc2a6c45e069798f65
DanilaTabulin <danila.tabulin@mail.ru>
2024-05-25 10:40:37 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication3.Repository;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using WebApplication3.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +19,14 @@ builder.Services.AddDbContextPool<AppDbContext>(options =>
 });
 builder.Services.AddScoped<IMovie, SQLMovie>();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = new PathString("/Account/Login");
+    });
+
+builder.Services.AddAntiforgery(options => options.HeaderName = "X-CSRF-TOKEN");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +40,25 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/chathub");
+
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding initial data");
+    }
+}
 
 app.Run();
